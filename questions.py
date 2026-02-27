08bcd249d6003e0e5fcd82f66d0a5c5a75386f69
lohithgowda405 <lohithgowdacheeku@gmail.com>
2026-02-27 11:36:52 +0530

    questions

---
+++ questions.py
@@ -0,0 +1,38 @@
+questions = [
+
+{
+"question":"What is the capital of India?",
+"options":["A Delhi","B Mumbai","C Chennai","D Kolkata"],
+"answer":"A"
+},
+
+{
+"question":"Which language is used for AI?",
+"options":["A Python","B HTML","C CSS","D SQL"],
+"answer":"A"
+},
+
+{
+"question":"Which keyword defines function?",
+"options":["A fun","B define","C def","D func"],
+"answer":"C"
+},
+
+{
+"question":"Mutable datatype?",
+"options":["A Tuple","B List","C String","D Int"],
+"answer":"B"
+},
+
+{
+"question":"Python creator?",
+"options":[
+"A Dennis Ritchie",
+"B Guido van Rossum",
+"C James Gosling",
+"D Elon Musk"
+],
+"answer":"B"
+}
+
+]
